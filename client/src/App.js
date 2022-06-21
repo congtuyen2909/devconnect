b034fa52fad98dff6705ecd44f4e22146a6f488e
congtuyen2909 <congtuyen295@gmail.com>
2022-06-21 10:32:21 +0700

    Protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { loadUser } from './actions/auth';
+import { Provider } from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
+import store from './store';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import { loadUser } from './actions/auth';
-import { Provider } from 'react-redux';
-import setAuthToken from './utils/setAuthToken';
-import store from './store';
 
 import './App.css';
 
@@ -32,7 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
-              <Route exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </>
