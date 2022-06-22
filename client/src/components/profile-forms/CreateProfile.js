56850075e836b6e76e62d21b1af4bd3e21795594
congtuyen2909 <congtuyen295@gmail.com>
2022-06-22 13:57:30 +0700

    Create profile component

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const CreateProfile = (props) => {
   const [formData, setFormData] = useState({
-    company: "",
-    website: "",
-    location: "",
-    status: "",
-    skills: "",
-    bio: "",
-    githubusername: "",
-    youtube: "",
-    facebook: "",
-    twitter: "",
-    linkedin: "",
-    instagram: "",
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    bio: '',
+    githubusername: '',
+    youtube: '',
+    facebook: '',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
   });
+  const [displaySocialNetwork, setDisplaySocialNetwork] = useState(false);
+
   const {
     company,
     website,
@@ -30,106 +33,193 @@ const CreateProfile = (props) => {
     linkedin,
     instagram,
   } = formData;
+
+  const handleChangeValueInput = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmitProfile = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
-      <h1 className="large text-primary">Create Your Profile</h1>
-      <p className="lead">
-        <i className="fas fa-user"></i> Let's get some information to make your
+      <h1 className='large text-primary'>Create Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Let's get some information to make your
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
-        <div className="form-group">
-          <select name="status">
-            <option value="0">* Select Professional Status</option>
-            <option value="Developer">Developer</option>
-            <option value="Junior Developer">Junior Developer</option>
-            <option value="Senior Developer">Senior Developer</option>
-            <option value="Manager">Manager</option>
-            <option value="Student or Learning">Student or Learning</option>
-            <option value="Instructor">Instructor or Teacher</option>
-            <option value="Intern">Intern</option>
-            <option value="Other">Other</option>
+      <form className='form' onSubmit={handleSubmitProfile}>
+        <div className='form-group'>
+          <select
+            name='status'
+            value={status}
+            onChange={handleChangeValueInput}
+          >
+            <option value='0'>* Select Professional Status</option>
+            <option value='Developer'>Developer</option>
+            <option value='Junior Developer'>Junior Developer</option>
+            <option value='Senior Developer'>Senior Developer</option>
+            <option value='Manager'>Manager</option>
+            <option value='Student or Learning'>Student or Learning</option>
+            <option value='Instructor'>Instructor or Teacher</option>
+            <option value='Intern'>Intern</option>
+            <option value='Other'>Other</option>
           </select>
-          <small className="form-text">
+          <small className='form-text'>
             Give us an idea of where you are at in your career
           </small>
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="Company" name="company" />
-          <small className="form-text">
+        <div className='form-group'>
+          <input
+            type='text'
+            value={company}
+            onChange={handleChangeValueInput}
+            placeholder='Company'
+            name='company'
+          />
+          <small className='form-text'>
             Could be your own company or one you work for
           </small>
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="Website" name="website" />
-          <small className="form-text">
+        <div className='form-group'>
+          <input
+            type='text'
+            value={website}
+            onChange={handleChangeValueInput}
+            placeholder='Website'
+            name='website'
+          />
+          <small className='form-text'>
             Could be your own or a company website
           </small>
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
-          <small className="form-text">
+        <div className='form-group'>
+          <input
+            type='text'
+            value={location}
+            onChange={handleChangeValueInput}
+            placeholder='Location'
+            name='location'
+          />
+          <small className='form-text'>
             City & state suggested (eg. Boston, MA)
           </small>
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="* Skills" name="skills" />
-          <small className="form-text">
+        <div className='form-group'>
+          <input
+            type='text'
+            value={skills}
+            onChange={handleChangeValueInput}
+            placeholder='* Skills'
+            name='skills'
+          />
+          <small className='form-text'>
             Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
           </small>
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="text"
-            placeholder="Github Username"
-            name="githubusername"
+            value={githubusername}
+            onChange={handleChangeValueInput}
+            type='text'
+            placeholder='Github Username'
+            name='githubusername'
           />
-          <small className="form-text">
+          <small className='form-text'>
             If you want your latest repos and a Github link, include your
             username
           </small>
         </div>
-        <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
-          <small className="form-text">Tell us a little about yourself</small>
+        <div className='form-group'>
+          <textarea
+            value={bio}
+            onChange={handleChangeValueInput}
+            placeholder='A short bio of yourself'
+            name='bio'
+          ></textarea>
+          <small className='form-text'>Tell us a little about yourself</small>
         </div>
 
-        <div className="my-2">
-          <button type="button" className="btn btn-light">
+        <div className='my-2'>
+          <button
+            type='button'
+            onClick={() => {
+              setDisplaySocialNetwork(!displaySocialNetwork);
+            }}
+            className='btn btn-light'
+          >
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
+        {displaySocialNetwork && (
+          <>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input
+                type='text'
+                value={twitter}
+                onChange={handleChangeValueInput}
+                placeholder='Twitter URL'
+                name='twitter'
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter" />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input
+                type='text'
+                value={facebook}
+                onChange={handleChangeValueInput}
+                placeholder='Facebook URL'
+                name='facebook'
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x'></i>
+              <input
+                type='text'
+                value={youtube}
+                onChange={handleChangeValueInput}
+                placeholder='YouTube URL'
+                name='youtube'
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input
+                type='text'
+                value={linkedin}
+                onChange={handleChangeValueInput}
+                placeholder='Linkedin URL'
+                name='linkedin'
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="Linkedin URL" name="linkedin" />
-        </div>
-
-        <div className="form-group social-input">
-          <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram" />
-        </div>
-        <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input
+                type='text'
+                value={instagram}
+                onChange={handleChangeValueInput}
+                placeholder='Instagram URL'
+                name='instagram'
+              />
+            </div>
+          </>
+        )}
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
-        </a>
+        </Link>
       </form>
     </>
   );
--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
 import { GET_PROFILE, PROFILE_ERROR } from './type';
 
 //get current user profile
@@ -15,4 +16,18 @@ export const getCurrentProfile = () => async (dispatch) => {
       payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
+};
+
+//create and update profile
+export const createProfile = (profile) => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  dispatch({
+    type: '',
+    payload: profile,
+  });
+
+  try {
+  } catch (err) {}
 };
